d433bfedecfd01ca7cb23a7f933b01203ef077cb
BantuManikyam <bantumanikyam179@gmail.com>
2024-05-29 08:46:53 +0530

    Commited by Manikyam

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -8,6 +8,30 @@ public class StudentGradeEvaluation {
 
         // Prompt the user to enter their name
         System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Name: "+name);
+        System.out.println("Enter age");
+        int age = scanner.nextInt();
+        System.out.println("Age: "+age);
+        System.out.println("Enter the score");
+        double score = scanner.nextDouble();
+        System.out.println("Score: "+score);
+        if (score>=90){
+            System.out.println("Grade: A");
+        }
+        else if (score>80){
+            System.out.println("Grade: B");   
+        }
+        else if (score>70){
+            System.out.println("Grade: C");
+        }
+        else if (score>60){
+            System.out.println("Grade: D");
+
+        }
+        else{
+            System.out.println("Grade: F");
+        }
         
 
         // Prompt the user to enter their age
